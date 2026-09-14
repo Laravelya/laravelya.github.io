30a3db7172e3d516c0f0e57b6ea3b60104398ea7
Febra <0xfebra@gmail.com>
2026-09-15 01:30:18 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Kalau server bermasalah atau koneksi putus, ubah CACHE_NAME untuk memaksa service worker menghapus cache lama dan memuat ulang aset baru.
-const CACHE_NAME = "eranga-cache-deployment-20260915-v2";
+const CACHE_NAME = "eranga-cache-deployment-20260915-v3";
 
 const STATIC_ASSETS = [
   "./",
