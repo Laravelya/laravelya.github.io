c25de0d7a2bd280a5d5aef913432b4763b02726f
Febra <0xfebra@gmail.com>
2026-09-15 01:10:59 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 // CONFIG
-const GAS_URL = "https://script.google.com/macros/s/AKfycbx0K3cWfijKJX0t2JbN-EBAI-6LQeT3W9ss7kntG4Wt5Ua8OY8g4HCGlPMl4fNov5vITw/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxmL6hVeVtCVpL-BvqUDyKDsBKldA41WLvpoHrFTdyKw8Toow9LEUQwR7Ec4qmZYqavHA/exec";
 const DAFTAR_BSSID_SEKOLAH = [
   "06:20:84:9a:42:1b",
   "06:20:84:aa:42:1b",
