4a8ad43188f75bb54d51c7613241d34b8a1b53ee
Febra <0xfebra@gmail.com>
2026-09-15 01:19:27 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "eranga-cache-deployment-20260915-v1";
+// Kalau server bermasalah atau koneksi putus, ubah CACHE_NAME untuk memaksa service worker menghapus cache lama dan memuat ulang aset baru.
+const CACHE_NAME = "eranga-cache-deployment-20260915-v2";
 
 const STATIC_ASSETS = [
   "./",
